3ea6ba0c043db4bdb5b4ca708ce1ab9bd197e080
Dope Beats <reweeden@alaska.edu>
2018-05-24 00:03:43 -0800

    Added the 2 other wikipedia examples as test cases.

--- sha1/test.py
+++ sha1/test.py
@@ -19,5 +19,13 @@ class SHA1TestCase(TestCase):
         h = hexlify(sha1(''))
         self.assertEqual(h, b'da39a3ee5e6b4b0d3255bfef95601890afd80709')
 
+    def test_1(self):
+        h = hexlify(sha1('The quick brown fox jumps over the lazy dog'))
+        self.assertEqual(h, b'2fd4e1c67a2d28fced849ee1bb76e7391b93eb12')
+
+    def test_2(self):
+        h = hexlify(sha1('The quick brown fox jumps over the lazy cog'))
+        self.assertEqual(h, b'de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3')
+
 if __name__ == '__main__':
     unittest.main()
